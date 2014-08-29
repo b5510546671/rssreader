f91eea1f54c61b1e580fba9fe1acbd2cea83f2d1
Supavit Kongwudhikunakorn <supavit_knot@yahoo.co.th>
2014-08-29 13:09:54 +0700

    built channel

--- src/model/Channel.java
+++ src/model/Channel.java
@@ -1,5 +1,76 @@
 package model;
 
-public class Channel {
+import java.util.*;
 
+public class Channel {
+	
+	private String title;
+	private String link;
+	private String description;
+	private String language;
+	private String lastBuildDate;
+	private String copyright;
+	
+	private List<Item> itemlist = new ArrayList<Item>();
+	
+	public Channel(){
+		
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public void setTitle(String title){
+		this.title = title;
+	}
+	
+	public String getLink(){
+		return link;
+	}
+	
+	public void setLink(String link){
+		this.link = link;
+	}
+	
+	public String getDescription(){
+		return description;
+	}
+	
+	public void setDescription(String description){
+		this.description = description;
+	}
+	
+	public String getLanguage(){
+		return language;
+	}
+	
+	public void setLanguage(String language){
+		this.language = language;
+	}
+
+	public String getLastBuildDate() {
+		return lastBuildDate;
+	}
+
+	public void setLastBuildDate(String lastBuildDate) {
+		this.lastBuildDate = lastBuildDate;
+	}
+
+	public String getCopyright() {
+		return copyright;
+	}
+
+	public void setCopyright(String copyright) {
+		this.copyright = copyright;
+	}
+
+	public List<Item> getItemlist() {
+		return itemlist;
+	}
+
+	public void setItemlist(List<Item> itemlist) {
+		this.itemlist = itemlist;
+	}
+	
 }
